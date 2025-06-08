837271891c657bbfd03fa2e94dc5017ebb3cf977
PS1hopat1na <holod3285@gmail.com>
2025-06-08 16:53:20 +0800

    Delete api directory

--- api/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from .routes import bp
-
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = 'your_secret_key'
-    app.register_blueprint(bp)
-    return app
