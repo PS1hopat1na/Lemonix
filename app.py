063687262791fcb666c32073996a444450e5a56e
PS1hopat1na <holod3285@gmail.com>
2025-06-08 16:53:37 +0800

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from api.routes import bp  # импортируем blueprint из routes.py
-
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-app.register_blueprint(bp)
-
-if __name__ == '__main__':
-    app.run(debug=True)
